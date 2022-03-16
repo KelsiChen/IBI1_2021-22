4443ac7eafc0c1e81c24429085dc06d0079c6e12
Tuhuanchang <501006056@qq.com>
2022-03-17 01:39:49 +0800

    Initialise

--- Practical5/triangle.py
+++ Practical5/triangle.py
@@ -1,5 +1,7 @@
 #sum = (1+n)*n/2
+#n is a variable in the range from 1 to 10, so I choose for loop.
 #let n run from 1 to 10, and stops at 11
+#if n is in the range(1,11),keep running; if n is out of the range,done.
 n= 1
 for n in range(1,11):
        sum = (1+n)*n/2
@@ -17,4 +19,4 @@ for n in range(1,11):
 #36
 #45
 #55
-#the #in front of numbers can be omitted
+#the "#" in front of numbers can be omitted
