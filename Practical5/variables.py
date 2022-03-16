477f1b8992de40c85f3ebfae84fa3ea51cf624c2
Tuhuanchang <501006056@qq.com>
2022-03-16 22:20:10 +0800

    Initialise

--- Practical5/variables.py
+++ Practical5/variables.py
@@ -1,3 +1,4 @@
+#3.1
 a = 19245301
 b = 4218520
 c = 271
@@ -5,4 +6,51 @@ d = b-c
 e = a-b
 print(d)
 print(e)
-print("Byebye")
+# d is 4218249, e is 15026781, e is greater than d, the rate of new cases is greater in 2021
+#3.2 Booleans
+
+X=""
+Y="something"
+W= X and Y
+print(W)
+#result is empty
+
+X = "something"
+Y = "something"
+W= X and Y
+print(W)
+#result is something
+
+X = "something"
+Y = "anything"
+W = X and Y
+print(W)
+#result is anything
+
+X = ""
+Y = "something"
+W= X or Y
+print(W)
+#result is something
+
+X = ""
+Y = ""
+W= X or Y
+print(W)
+#result is empty
+
+X = "something"
+Y = "something"
+W= X or Y
+print(W)
+#result is something
+
+X = "something"
+Y = "anything"
+W = X or Y
+print(W)
+#result is something
+
+
+
+
