6423275d42c2124cdf66c8a6b7e45c5f9cb53caa
Tuhuanchang <501006056@qq.com>
2022-03-16 22:39:32 +0800

    Initialise

---
+++ Practical5/triangle.py
@@ -0,0 +1,20 @@
+#sum = (1+n)*n/2
+#let n run from 1 to 10, and stops at 11
+n= 1
+for n in range(1,11):
+       sum = (1+n)*n/2
+       n= n+1
+       print(sum)
+#in the range, 1 is included, 11 is not included
+#the result is :
+#1
+#3
+#6
+#10
+#15
+#21
+#28
+#36
+#45
+#55
+#the #in front of numbers can be omitted
