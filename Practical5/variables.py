b5f15049680ce8bd63740b8c9307dfcf9c108a1b
Tuhuanchang <501006056@qq.com>
2022-04-19 17:55:59 +0800

    Update variables.py

--- Practical5/variables.py
+++ Practical5/variables.py
@@ -4,61 +4,24 @@ b = 4218520
 c = 271
 d = b-c
 e = a-b
-print(d)
-print(e)
+
+if d < e:
+  print("The rate of new cases was greater in 2021")
+else:
+  print("The rate of new cases was greater in 2020")
 # d is 4218249, e is 15026781, e is greater than d.
 #the rate of new cases is greater in 2021
 #2021 has the greatest number of new COVID-19 cases.
 
-#3.2 Booleans
 
-X = ""
-Y = ""
-W= X and Y
-print(W)
-#result is empty
-
-X=""
-Y="something"
-W= X and Y
-print(W)
-#result is empty
-
-X = "something"
-Y = "something"
-W= X and Y
-print(W)
-#result is "something"
-
-X = "something"
-Y = "anything"
+# 3.2 Booleans
+X = input()
+Y = input()
+# input 2 boolean values X and Y
 W = X and Y
-print(W)
-#result is "anything"
-
-X = ""
-Y = "something"
-W= X or Y
-print(W)
-#result is "something"
-
-X = ""
-Y = ""
-W= X or Y
-print(W)
-#result is empty
-
-X = "something"
-Y = "something"
-W= X or Y
-print(W)
-#result is "something"
-
-X = "something"
-Y = "anything"
-W = X or Y
-print(W)
-#result is "something"
+# set the boolean value of W
+print (W)
+# output the answer
 
 
 
