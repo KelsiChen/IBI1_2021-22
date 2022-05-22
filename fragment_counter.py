3a5e3592e98510270b5eafe738a6a67e3f0f874f
Tuhuanchang <101197963+Tuhuanchang@users.noreply.github.com>
2022-05-22 16:00:53 +0800

    Delete fragment_counter.py

--- fragment_counter.py
+++
@@ -1,4 +0,0 @@
-seq = 'ATGCAATCGACTACGATCAATCGAGGGCC'
-#First,figure out the number of EcoRI sequence, number of fragment is number of sequence plus one.
-fragment=seq.count('GAATTC')+1
-print("The number of fragments generated is", fragment)
