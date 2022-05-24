d747df2f6be5713dd8826eb1990244ddcb7ccb91
Tuhuanchang <501006056@qq.com>
2022-05-24 13:17:39 +0800

    Second submission of XMLsolution

--- Practical14/XMLsolution.py
+++ Practical14/XMLsolution.py
@@ -1,4 +1,3 @@
-from collections import Counter
 from xml.dom.minidom import parse
 import xml.dom.minidom
 import matplotlib.pyplot as plt
@@ -10,67 +9,83 @@ collection = DOMTree.documentElement
 terms = collection.getElementsByTagName("term")
 
 
-#Get the total number of terms.
-length = terms.length
-print("The total number of nodes is", length)
+#We need to find all child nodes of one node and its child nodes until it reaches the bottom.
+#So we can get the number through calculating the number of parent nodes of the child nodes.
+#This function can get the id  of parent nodes
+def find_parents(node_id, parent_ids):
+    mediate_ids = nodefamily[node_id]
+    for mediate_id in mediate_ids:
+        parent_ids.add(mediate_id) 
+        find_parents(mediate_id, parent_ids)
 
 
-#Get the distribution of child nodes across terms.
-number = []
-for term in terms: 
-    is_a = term.getElementsByTagName("is_a")
-    node= len(is_a)
-    number.append(node)
+nodefamily = {}
+result_all = {}
 
+#We need to get all nodes of each term 
+#The information will be stored in the nodefamily.
+for term in terms:  
+    node_id = term.getElementsByTagName("id")[0].childNodes[0].data 
+    mediate_ids = [] #对于父节点
+    for is_a in term.getElementsByTagName("is_a"): 
+        mediate_ids.append(is_a.childNodes[0].data) 
+    nodefamily[node_id] = mediate_ids 
+    result_all[node_id] = 0
 
-count = Counter(number)
-print(count)
-#The result is Counter({1: 23733, 2: 14172, 3: 4758, 0: 2294, 4: 1490, 5: 565, 6: 221, 7: 78, 8: 20, 9: 7, 10: 2})
+#Now we need to calculate all parent nodes of each child node.
+#This equals the number of all child nodes of each parent node.
+for key in nodefamily.keys():
+    parent_ids = set()
+    find_parents(key, parent_ids)
+    for parent_id in parent_ids:
+        result_all[parent_id] += 1
 
-#Draw the first chart.
-nodenumber1=[0,1,2,3,4,5,6,7,8,9,10]
-nodecount1=[2294,23733,14172,4758,1490,565,221,78,20,7,2]
-
-x= nodenumber1
-y= nodecount1
-
-plt.bar(x,y,align='center',color='c',tick_label= [chr(i) for i in range(65, 75)])
-plt.xlabel('The number of childnodes of each term')
-plt.ylabel('The count of terms')
+#Draw the plot of distribution of terms
+plt.boxplot(result_all.values(), vert=True, whis=1.5, patch_artist=True, showbox=True, showcaps=True, showfliers=True)
 plt.title('The distribution of child nodes across all terms in the Gene Ontology')
+plt.xlabel("All terms in the Gene Ontology")
+plt.ylabel("The number of childnodes of all terms in the Gene Ontology")
 plt.show()
 
 
-#Get the number of terms associated with 'translation'.
+
+#Get the list of terms associated with 'translation'.
 term_list = []
 for term in terms:
       defstr_text = term.getElementsByTagName("defstr")[0].childNodes[0]
-      lines = defstr_text.data
-      if 'translation' in lines:
+      if 'translation' in defstr_text.data:
           term_list.append(term)
 
-number2 = []
-for term in term_list: 
-    is_a2 = term.getElementsByTagName("is_a")
-    node2= len(is_a2)
-    number2.append(node2)
+#Find the nodes with translation in the nodefamily
+node_list = []
+for term in term_list:  
+    node_id = term.getElementsByTagName("id")[0].childNodes[0].data 
+    node_list.append(node_id)
 
-count2 = Counter(number2)
-print(count2)
-#The result is "Counter({1: 128, 2: 117, 3: 37, 0: 9, 4: 3, 5: 1, 6: 1})"
+result_translation = []
+for node in node_list:
+      result_translation.append(result_all[node])
 
-#Draw the second distribution chart.
-a=[0,1,2,3,4,5,6]
-b=[9,128,117,37,3,1,1]
-
-plt.bar(a,b,align='center',color='c',tick_label= [chr(i) for i in range(65, 75)])
-plt.xlabel('The number of childnodes of terms associated with "translation"')
-plt.ylabel('The count of terms')
-plt.title('The distribution of child nodes across terms associated with "translation" ')
+#Draw the distribution plot
+plt.boxplot(result_translation, vert=True, whis=1.5, patch_artist=True, showbox=True, showcaps=True, showfliers=True)
+plt.title('The distribution of child nodes across terms associated with translation ')
+plt.xlabel("Terms associated with translation")
+plt.ylabel("number of childnodes of terms with translation")
 plt.show()
 
-#Conclusion
-#The overall Gene Ontology terms have 1.63 child nodes on average.
-#The 'translation' terms have 1.68 child nodes on average.
-#The 'translation' terms contain a greater number of child nodes than the overall Gene Ontology.
 
+#Calculate the average
+average_all= sum(result_all.values())/len(result_all.values())
+average_translation= sum(result_translation)/len(result_translation)
+print("The overall Gene Ontology terms have", average_all, "child nodes on average.")
+print("The 'translation' terms have", average_translation, "child nodes on average.")
+
+if average_all > average_translation:
+   print("The 'translation' terms contain a greater number of child nodes than the overall Gene Ontology terms.")
+else:
+   print("The overall Gene Ontology terms contain a greater number of child nodes than the 'translation' terms.")
+
+
+#The overall Gene Ontology terms have 12.08177017321504 child nodes on average.
+#The 'translation' terms have 13.486486486486486 child nodes on average.
+#The overall Gene Ontology terms contain a greater number of child nodes than the 'translation' terms.
