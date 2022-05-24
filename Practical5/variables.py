f40db8bb9b405d35ea28a0693b47e0ed4ad71e45
Tuhuanchang <501006056@qq.com>
2022-05-24 21:49:36 +0800

    Correcting variables.py

--- Practical5/variables.py
+++ Practical5/variables.py
@@ -22,7 +22,9 @@ W = X and Y
 # set the boolean value of W
 print (W)
 # output the answer
-
-
-
+# When I input X = True, Y = True, W is True.
+# When I input X = True, Y = False, W is False.
+# When I input X = False, Y = False, W is False.
+# When I input X = False, Y = True, W is False.
+# The results meet the principle of booleans in python.
 
